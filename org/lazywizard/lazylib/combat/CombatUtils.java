a11b48fc917687340bc9b00d93a06682ad43aabf
Rob Maguire <iu65a@hotmail.com>
2012-12-08 19:23:22 -0600

    Now uses a combat plugin to avoid the need to pass CombatEngineAPI in parameters

--- org/lazywizard/lazylib/combat/CombatUtils.java
+++ org/lazywizard/lazylib/combat/CombatUtils.java
@@ -5,15 +5,20 @@ import com.fs.starfarer.api.combat.BoundsAPI.SegmentAPI;
 import com.fs.starfarer.api.combat.CombatEngineAPI;
 import com.fs.starfarer.api.combat.CombatEntityAPI;
 import com.fs.starfarer.api.combat.DamagingProjectileAPI;
+import com.fs.starfarer.api.combat.EveryFrameCombatPlugin;
 import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.input.InputEventAPI;
 import java.util.ArrayList;
 import java.util.List;
 import org.lazywizard.lazylib.BaseUtils;
 import org.lazywizard.lazylib.Line;
 import org.lwjgl.util.vector.Vector2f;
 
-public class CombatUtils
+public class CombatUtils implements EveryFrameCombatPlugin
 {
+    private static CombatEngineAPI engine;
+    private static float combatTime;
+
     public static enum DefenseType
     {
         HULL,
@@ -28,7 +33,17 @@ public class CombatUtils
         return new Line(segment.getP1(), segment.getP2());
     }
 
-    public static List<DamagingProjectileAPI> getProjectilesWithinRange(Vector2f location, float range, CombatEngineAPI engine)
+    public static CombatEngineAPI getCombatEngine()
+    {
+        return engine;
+    }
+
+    public static float getElapsedCombatTime()
+    {
+        return combatTime;
+    }
+
+    public static List<DamagingProjectileAPI> getProjectilesWithinRange(Vector2f location, float range)
     {
         List<DamagingProjectileAPI> projectiles = new ArrayList<DamagingProjectileAPI>();
 
@@ -43,7 +58,7 @@ public class CombatUtils
         return projectiles;
     }
 
-    public static List<ShipAPI> getShipsWithinRange(Vector2f location, float range, CombatEngineAPI engine)
+    public static List<ShipAPI> getShipsWithinRange(Vector2f location, float range)
     {
         List<ShipAPI> ships = new ArrayList<ShipAPI>();
 
@@ -58,7 +73,7 @@ public class CombatUtils
         return ships;
     }
 
-    public static List<BattleObjectiveAPI> getObjectivesWithinRange(Vector2f location, float range, CombatEngineAPI engine)
+    public static List<BattleObjectiveAPI> getObjectivesWithinRange(Vector2f location, float range)
     {
         List<BattleObjectiveAPI> objectives = new ArrayList<BattleObjectiveAPI>();
 
@@ -73,7 +88,7 @@ public class CombatUtils
         return objectives;
     }
 
-    public static List<CombatEntityAPI> getEntitiesWithinRange(Vector2f location, float range, CombatEngineAPI engine)
+    public static List<CombatEntityAPI> getEntitiesWithinRange(Vector2f location, float range)
     {
         List<CombatEntityAPI> entities = new ArrayList<CombatEntityAPI>();
 
@@ -110,5 +125,18 @@ public class CombatUtils
         }
 
         return entities;
+    }
+
+    @Override
+    public void advance(float amount, List<InputEventAPI> events)
+    {
+        combatTime += amount;
+    }
+
+    @Override
+    public void init(CombatEngineAPI engine)
+    {
+        CombatUtils.engine = engine;
+        combatTime = 0f;
     }
 }
