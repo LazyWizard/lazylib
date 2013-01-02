a2733d60b19af2d86cadcc23c5e1c827f1f4dd31
Robert Maguire <LazyWizard@users.noreply.github.com>
2013-01-02 13:46:49 -0600

    Started work on campaign utils

---
+++ org/lazywizard/lazylib/campaign/CargoUtils.java
@@ -0,0 +1,96 @@
+package org.lazywizard.lazylib.campaign;
+
+import com.fs.starfarer.api.campaign.CargoAPI;
+import com.fs.starfarer.api.campaign.CargoStackAPI;
+
+public class CargoUtils
+{
+    public static void moveStack(CargoStackAPI stack, CargoAPI from, CargoAPI to)
+    {
+        to.addItems(stack.getType(), stack.getData(), stack.getSize());
+        from.removeItems(stack.getType(), stack.getData(), stack.getSize());
+    }
+
+    public static void moveCargo(CargoAPI from, CargoAPI to)
+    {
+        for (CargoStackAPI stack : from.getStacksCopy())
+        {
+            moveStack(stack, from, to);
+        }
+    }
+
+    public static float getSpaceTakenByWeapons(CargoAPI cargo)
+    {
+        float totalSpace = 0f;
+
+        for (CargoStackAPI stack : cargo.getStacksCopy())
+        {
+            if (stack.isWeaponStack())
+            {
+                totalSpace += stack.getCargoSpace();
+            }
+        }
+
+        return totalSpace;
+    }
+
+    public static float getSpaceTakenByCrew(CargoAPI cargo)
+    {
+        float totalSpace = 0f;
+
+        for (CargoStackAPI stack : cargo.getStacksCopy())
+        {
+            if (stack.isCrewStack())
+            {
+                totalSpace += stack.getCargoSpace();
+            }
+        }
+
+        return totalSpace;
+    }
+
+    public static float getSpaceTakenBySupplies(CargoAPI cargo)
+    {
+        float totalSpace = 0f;
+
+        for (CargoStackAPI stack : cargo.getStacksCopy())
+        {
+            if (stack.isSupplyStack())
+            {
+                totalSpace += stack.getCargoSpace();
+            }
+        }
+
+        return totalSpace;
+    }
+
+    public static float getSpaceTakenByFuel(CargoAPI cargo)
+    {
+        float totalSpace = 0f;
+
+        for (CargoStackAPI stack : cargo.getStacksCopy())
+        {
+            if (stack.isFuelStack())
+            {
+                totalSpace += stack.getCargoSpace();
+            }
+        }
+
+        return totalSpace;
+    }
+
+    public static float getSpaceTakenByResources(CargoAPI cargo)
+    {
+        float totalSpace = 0f;
+
+        for (CargoStackAPI stack : cargo.getStacksCopy())
+        {
+            if (stack.isResourceStack())
+            {
+                totalSpace += stack.getCargoSpace();
+            }
+        }
+
+        return totalSpace;
+    }
+}
