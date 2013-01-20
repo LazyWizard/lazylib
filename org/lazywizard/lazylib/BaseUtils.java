93192ef1a3bd4819ae978281368d3c54c41c5edf
Robert Maguire <LazyWizard@users.noreply.github.com>
2013-01-19 22:54:20 -0600

    Added weighted random method to BaseUtils

--- org/lazywizard/lazylib/BaseUtils.java
+++ org/lazywizard/lazylib/BaseUtils.java
@@ -1,9 +1,31 @@
 package org.lazywizard.lazylib;
 
-import java.util.Collection;
+import java.util.*;
 
 public class BaseUtils
 {
+    public static <T> T weightedRandom(Map<T, Float> pickFrom)
+    {
+        float totalWeight = 0.0f;
+        for (Float tmp : pickFrom.values())
+        {
+            totalWeight += tmp;
+        }
+
+        float random = (float) Math.random() * totalWeight;
+        for (Map.Entry<T, Float> tmp : pickFrom.entrySet())
+        {
+            random -= tmp.getValue();
+
+            if (random <= 0.0f)
+            {
+                return tmp.getKey();
+            }
+        }
+
+        return null;
+    }
+
     public static String implode(Collection<String> toImplode, String separator)
     {
         if (toImplode.isEmpty())
