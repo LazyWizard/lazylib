df6c52b09b26d3750b75f15c106f7fda2222da70
LazyWizard <iu65a@hotmail.com>
2013-12-18 18:49:31 -0600

    Added getAlliesInArc() and getNearestAllyInArc() to WeaponUtils

--- org/lazywizard/lazylib/combat/WeaponUtils.java
+++ org/lazywizard/lazylib/combat/WeaponUtils.java
@@ -103,6 +103,74 @@ public class WeaponUtils
     }
 
     /**
+     * Find the closest ally in range of a {@link WeaponAPI}.
+     *
+     * @param weapon The {@link WeaponAPI} to search around.
+     * <p>
+     * @return The allied {@link ShipAPI} closest to {@code weapon}, or
+     *         {@code null} if none are in range.
+     * <p>
+     * @since 1.4
+     */
+    public static ShipAPI getNearestAllyInArc(WeaponAPI weapon)
+    {
+        ShipAPI closest = null;
+        float maxRange = weapon.getRange() * weapon.getRange();
+        float distanceSquared, closestDistanceSquared = Float.MAX_VALUE;
+
+        for (ShipAPI tmp : Global.getCombatEngine().getShips())
+        {
+            if (tmp.getOwner() != weapon.getShip().getOwner()
+                    || weapon.distanceFromArc(tmp.getLocation()) > 0f)
+            {
+                continue;
+            }
+
+            distanceSquared = MathUtils.getDistanceSquared(tmp.getLocation(),
+                    weapon.getLocation());
+
+            if (distanceSquared > maxRange)
+            {
+                continue;
+            }
+
+            if (distanceSquared < closestDistanceSquared)
+            {
+                closest = tmp;
+                closestDistanceSquared = distanceSquared;
+            }
+        }
+
+        return closest;
+    }
+
+    /**
+     * Finds all allied ships within range of a {@link WeaponAPI}.
+     *
+     * @param weapon The weapon to detect allies in range of.
+     * <p>
+     * @return A {@link List} containing all allied ships within range.
+     * <p>
+     * @since 1.4
+     */
+    public static List<ShipAPI> getAlliesInArc(WeaponAPI weapon)
+    {
+        List<ShipAPI> allies = new ArrayList<ShipAPI>();
+        float range = weapon.getRange();
+
+        for (ShipAPI ship : AIUtils.getAlliesOnMap(weapon.getShip()))
+        {
+            if (MathUtils.getDistance(ship, weapon.getLocation()) <= range
+                    && weapon.distanceFromArc(ship.getLocation()) == 0f)
+            {
+                allies.add(ship);
+            }
+        }
+
+        return allies;
+    }
+
+    /**
      * Find the closest enemy in range of a {@link WeaponAPI}.
      *
      * @param weapon The {@link WeaponAPI} to search around.
