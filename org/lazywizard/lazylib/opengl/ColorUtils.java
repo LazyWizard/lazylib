6abe23817a5e70b7fb4df37aaccb57309fac2e9e
LazyWizard <iu65a@hotmail.com>
2014-07-04 18:00:33 -0500

    Added missing JavaDoc

--- org/lazywizard/lazylib/opengl/ColorUtils.java
+++ org/lazywizard/lazylib/opengl/ColorUtils.java
@@ -4,13 +4,27 @@ import java.awt.Color;
 import static org.lwjgl.opengl.GL11.*;
 
 /**
+ * Contains methods for working with OpenGL and AWT color objects.
  *
  * @author LazyWizard
  * @since 1.9
  */
-// TODO: Javadoc and add to changelog
 public class ColorUtils
 {
+    /**
+     * Sets the OpenGL color using an AWT {@link Color} object.
+     * <p>
+     * @param color                 The color to set.
+     * @param alphaMult             Multiplies the color's alpha channel with
+     *                              this, or replaces it if
+     *                              {@code overrideOriginalAlpha} is
+     *                              {@code true}.
+     * @param overrideOriginalAlpha Whether to completely override
+     *                              {@code color}'s alpha channel with
+     *                              {@code alphaMult}.
+     * <p>
+     * @since 1.9
+     */
     public static void glColor(Color color, float alphaMult,
             boolean overrideOriginalAlpha)
     {
@@ -19,6 +33,13 @@ public class ColorUtils
                 ? alphaMult : (color.getAlpha() * alphaMult))));
     }
 
+    /**
+     * Sets the OpenGL color using an AWT {@link Color} object.
+     * <p>
+     * @param color The color to set.
+     * <p>
+     * @since 1.9
+     */
     public static void glColor(Color color)
     {
         glColor4ub((byte) color.getRed(), (byte) color.getGreen(),
